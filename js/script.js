bc2d9bccd4f77e59f0e7ff39abb1e699ad37723d
Giorgio <shadigiorgioislam@gmail.com>
2021-02-02 10:25:54 +0100

    Add v-for

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+  el: '#users',
+  data: {
+    contacts:[
+      {
+      name: 'Michele',
+      avatar: '_1',
+      },
+      {
+      name: 'Fabio',
+      avatar: '_2',
+    },
+    {
+      name: 'Samuele',
+      avatar: '_3',
+    },
+    {
+      name: 'Luciano',
+      avatar: '_4',
+    },
+    {
+      name: 'Marco',
+      avatar: '_5',
+    },
+    {
+      name: 'Anna',
+      avatar: '_6',
+    },
+    {
+      name: 'Sandro',
+      avatar: '_7',
+    },
+    {
+      name: 'Mario',
+      avatar: '_8',
+    }
+    ]
+  }
+})
