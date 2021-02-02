f73d87047ee4cf62c678e164838a1e5dcd3ed78c
Giorgio <shadigiorgioislam@gmail.com>
2021-02-02 15:55:26 +0100

    Add chat  conversation

--- js/script.js
+++ js/script.js
@@ -1,39 +1,63 @@
 var app = new Vue({
-  el: '#users',
+  el: '#container',
   data: {
     contacts:[
       {
       name: 'Michele',
       avatar: '_1',
+      message: 'ok'
       },
       {
       name: 'Fabio',
       avatar: '_2',
-    },
-    {
-      name: 'Samuele',
-      avatar: '_3',
-    },
-    {
-      name: 'Luciano',
-      avatar: '_4',
-    },
-    {
-      name: 'Marco',
-      avatar: '_5',
-    },
-    {
-      name: 'Anna',
-      avatar: '_6',
-    },
-    {
-      name: 'Sandro',
-      avatar: '_7',
-    },
-    {
-      name: 'Mario',
-      avatar: '_8',
+      message: 'ok'
+      },
+      {
+        name: 'Samuele',
+        avatar: '_3',
+        message: 'ok'
+      },
+      {
+        name: 'Luciano',
+        avatar: '_4',
+        message: 'ok'
+      },
+      {
+        name: 'Marco',
+        avatar: '_5',
+        message: 'ok'
+      },
+      {
+        name: 'Anna',
+        avatar: '_6',
+        message: 'ok'
+      },
+      {
+        name: 'Sandro',
+        avatar: '_7',
+        message: 'ok'
+      },
+      {
+        name: 'Mario',
+        avatar: '_8',
+        message: 'ok'
+      }
+    ],
+    searched: '',
+    chat: '',
+    sent: [],
+    riceved: [],
+    stato: 'Online'
+  },
+  methods: {
+    sentMessage(array) {
+      this.sent.push(array);
+      this.stato = 'Sta scrivendo...';
+      setTimeout(()=> {
+        this.riceved.push('Ok');
+        this.stato = 'Online';
+        console.log(this.riceved);
+      }, 3000);
     }
-    ]
   }
 })
