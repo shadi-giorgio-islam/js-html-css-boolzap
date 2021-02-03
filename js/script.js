db36f68a300d543da0e2fd1103ccc47e7baa45f0
Giorgio <shadigiorgioislam@gmail.com>
2021-02-03 10:46:38 +0100

    Add chat for each contact

--- js/script.js
+++ js/script.js
@@ -5,58 +5,79 @@ var app = new Vue({
       {
       name: 'Michele',
       avatar: '_1',
-      message: 'ok'
+      message: 'ok',
+      sent: [],
+      chat: '',
+      riceved: []
       },
       {
       name: 'Fabio',
       avatar: '_2',
-      message: 'ok'
+      message: 'ok',
+      sent: [],
+      chat: '',
+      riceved: []
       },
       {
         name: 'Samuele',
         avatar: '_3',
-        message: 'ok'
+        message: 'ok',
+        sent: [],
+        chat: '',
+        riceved: []
       },
       {
         name: 'Luciano',
         avatar: '_4',
-        message: 'ok'
+        message: 'ok',
+        sent: [],
+        chat: '',
+        riceved: []
       },
       {
         name: 'Marco',
         avatar: '_5',
-        message: 'ok'
+        message: 'ok',
+        sent: [],
+        chat: '',
+        riceved: []
       },
       {
         name: 'Anna',
         avatar: '_6',
-        message: 'ok'
+        message: 'ok',
+        sent: [],
+        chat: '',
+        riceved: []
       },
       {
         name: 'Sandro',
         avatar: '_7',
-        message: 'ok'
+        message: 'ok',
+        sent: [],
+        chat: '',
+        riceved: []
       },
       {
         name: 'Mario',
         avatar: '_8',
-        message: 'ok'
+        message: 'ok',
+        sent: [],
+        chat: '',
+        riceved: []
       }
     ],
     searched: '',
-    chat: '',
-    sent: [],
-    riceved: [],
     stato: 'Online',
     indice: 0
   },
   methods: {
     sentMessage(array) {
-      this.sent.push(array);
+      this.contacts[this.indice].sent.push(array);
       this.stato = 'Sta scrivendo...';
-      this.riceved.push('...');
+      this.contacts[this.indice].riceved.push('...');
       setTimeout(()=> {
-        this.riceved[this.riceved.length - 1]= 'Ok';
+        this.contacts[this.indice].riceved[this.contacts[this.indice].riceved.length - 1]= 'Ok';
         this.stato = 'Online';
         console.log(this.riceved);
       }, 3000);
